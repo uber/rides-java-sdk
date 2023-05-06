c028ffb3b374665ebd2a7ba773961ddc2f1278bc
Saurabh Lalwani <lalwani@uber.com>
2023-05-05 20:14:51 -0700

    added javadoc to ProfileHint class

--- uber-core/src/main/java/com/uber/sdk/core/auth/ProfileHint.java
+++ uber-core/src/main/java/com/uber/sdk/core/auth/ProfileHint.java
@@ -6,7 +6,118 @@ import java.io.Serializable;
 
 import javax.annotation.Nonnull;
 
-public class ProfileHint implements Serializable {
+/**
+ * {@Link #ProfileHint} is builder to setup user's personal information like phone number, email
+ * firstName and lastName
+ */
+final public class ProfileHint implements Serializable {
+    /**
+     * Builder class for {@link ProfileHint}
+     */
+    public static class Builder {
+        private String firstName;
+        private String lastName;
+        private String email;
+        private String phone;
+
+        /**
+         * Set first name
+         *
+         * @param firstName first name of the user
+         */
+        public Builder firstName(@Nonnull String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        /**
+         * Set last name
+         *
+         * @param lastName last name of the user
+         */
+        public Builder lastName(@Nonnull String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        /**
+         * Set email address
+         *
+         * @param email email address of the user
+         */
+        public Builder email(@Nonnull String email) {
+            this.email = email;
+            return this;
+        }
+
+        /**
+         * Set phone number as a string including country code
+         *
+         * @param phone phone number of the user
+         */
+        public Builder phone(@Nonnull String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public ProfileHint build() {
+            return new ProfileHint(
+                    firstName,
+                    lastName,
+                    email,
+                    phone
+            );
+        }
+    }
+
+    /**
+     * Gets the first name of the user
+     *
+     * @return The first name of the user if set, null otherwise
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Gets the last name of the user
+     *
+     * @return The last name of the user if set, null otherwise
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Gets the email address of the user
+     *
+     * @return The email address of the user if set, null otherwise
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Gets the phone number of the user as a string including country code
+     *
+     * @return The phone number of the user if set, null otherwise
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * Returns new Builder with the current instance's properties as default values
+     *
+     * @return a new Builder object
+     */
+    public Builder newBuilder() {
+        return new Builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .email(email)
+                .phone(phone);
+    }
 
     @Json(name = "first_name")
     private final String firstName;
@@ -26,64 +137,5 @@ public class ProfileHint implements Serializable {
         this.lastName = lastName;
         this.email = email;
         this.phone = phone;
-    }
-
-    public static class Builder {
-        private String firstName;
-        private String lastName;
-        private String email;
-        private String phone;
-        public Builder firstName(@Nonnull String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-
-        public Builder lastName(@Nonnull String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-
-        public Builder email(@Nonnull String email) {
-            this.email = email;
-            return this;
-        }
-
-        public Builder phone(@Nonnull String phone) {
-            this.phone = phone;
-            return this;
-        }
-
-        public ProfileHint build() {
-            return new ProfileHint(
-                    firstName,
-                    lastName,
-                    email,
-                    phone
-            );
-        }
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public Builder newBuilder() {
-        return new Builder()
-                .firstName(firstName)
-                .lastName(lastName)
-                .email(email)
-                .phone(phone);
     }
 }
